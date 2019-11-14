e4f8fff1f5a61132a3da0c719005f81fc3557dbb
wxj789 <1105747245@qq.com>
2019-11-14 20:37:03 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    主页
+    <el-container>
+      <el-aside>
+        <div class="aside">
+          <!-- <img src="../../assets/img/logo_admin.png" alt=""> -->
+        </div>
+      </el-aside>
+      <el-container>
+        <el-header>头部</el-header>
+        <el-main>主内容</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -12,3 +22,9 @@ export default {
   }
 }
 </script>
+<style lang="less" scoped>
+  // .aside {
+    // background-color:#000;
+// }
+
+</style>
