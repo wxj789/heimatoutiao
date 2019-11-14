35c2d5a2f6b920e376876411467d8915ba7ba012
wxj789 <1105747245@qq.com>
2019-11-14 21:36:24 +0800

    黑马头条PC-主页模块-左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,34 @@
 <template>
   <div>
     <el-container>
-      <el-aside>
+      <!-- 左侧导航 -->
+      <el-aside style="width:220px">
         <div class="aside">
-          <!-- <img src="../../assets/img/logo_admin.png" alt=""> -->
+          <img src="../../assets/img/logo_admin.png" alt="">
         </div>
+        <!-- 左侧菜单 -->
+        <el-menu style="width:221px" background-color='#353b4e' text-color='#adafb5' active-text-color="#ffd04b">
+          <el-menu-item>首页</el-menu-item>
+          <!-- 二级菜单 -->
+          <el-submenu>
+            <template slot="title">内容管理</template>
+            <el-menu-item>发布文章</el-menu-item>
+            <el-menu-item>内容列表</el-menu-item>
+            <el-menu-item>评论列表</el-menu-item>
+            <el-menu-item>素材管理</el-menu-item>
+          </el-submenu>
+          <!-- 二级菜单 -->
+          <el-submenu>
+            <template slot="title">粉丝管理</template>
+            <el-menu-item>图文数据</el-menu-item>
+            <el-menu-item>粉丝概况</el-menu-item>
+            <el-menu-item>粉丝画像</el-menu-item>
+            <el-menu-item>粉丝列表</el-menu-item>
+          </el-submenu>
+          <el-menu-item>账户信息</el-menu-item>
+        </el-menu>
       </el-aside>
+      <!-- 右侧 -->
       <el-container>
         <el-header>头部</el-header>
         <el-main>主内容</el-main>
@@ -23,8 +46,23 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-  // .aside {
-    // background-color:#000;
-// }
-
+  .el-aside {
+    background: #323745;
+    height: 100vh;
+    overflow: hidden;
+    .aside {
+      height: 60px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      img {
+        height: 30px;
+      }
+    }
+    // .menu {
+    //   background-color: #353b4e;
+    //   text-color: #adafb5;
+    //   // :active:#aaa
+    // }
+  }
 </style>
