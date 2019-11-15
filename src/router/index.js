f65bc059556db473f5e6e5704ff044b4eaed0587
wxj789 <1105747245@qq.com>
2019-11-15 11:39:16 +0800

    loyout组件-左侧导航与右侧二级路由进行配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,49 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/home/index.vue' // 完整路径
-import Home from '../views/home' // 简写路径
+import layout from '../views/layout' // 简写路径
 import login from '../views/login' // 简写路径
+// @ 是 VueCLI 中提供的一种特殊的路径规则，直接指向 src 目录路径
+// 注意：在VueCLI 创建的子昂木中，无论在哪里使用 @ 符号， 都指向src
+import home from '@/views/home'
+import article from '@/views/article'
+import publish from '@/views/publish'
 
 Vue.use(VueRouter)
 
-const routes = [{
-  path: '/',
-  redirect: '/login'
-},
+const routes = [
+//   {
+//   path: '/',
+//   redirect: '/login'
+// },
 // 一级路由  主页
-{
-  path: '/home',
-  name: 'home',
-  component: Home
-},
-// 一级路由  登录页
-{
-  path: '/login',
-  component: login
-}
+  {
+    path: '/',
+    // name: 'home',
+    component: layout,
+    children: [
+      // 首页
+      {
+        path: '', // 默认子路由
+        component: home
+      },
+      // 文章列表
+      {
+        path: '/article',
+        component: article
+      },
+      // 发表文章
+      {
+        path: '/publish',
+        component: publish
+      }
+    ]
+  },
+  // 一级路由  登录页
+  {
+    path: '/login',
+    component: login
+  }
   // {
   //   path: '/about',
   //   name: 'about',
---
+++ src/views/layout/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <el-container>
+      <!-- 左侧导航 -->
+      <el-aside style="width:220px">
+        <div class="aside">
+          <img src="../../assets/img/logo_admin.png" alt="">
+        </div>
+        <!-- 左侧菜单 -->
+        <!-- 添加router属性  启用路由模式 该模式会在激活导航时以 index 作为 path 进行路由跳转 -->
+        <el-menu
+          router
+          style="width:221px"
+          background-color='#353b4e'
+          text-color='#adafb5'
+          active-text-color="#ffd04b"
+        >
+          <el-menu-item index="/">首页</el-menu-item>
+          <!-- 二级菜单 -->
+          <el-submenu index='1'>
+            <template slot="title">内容管理</template>
+            <el-menu-item index="/publish">发布文章</el-menu-item>
+            <el-menu-item index="article">内容列表</el-menu-item>
+            <el-menu-item>评论列表</el-menu-item>
+            <el-menu-item>素材管理</el-menu-item>
+          </el-submenu>
+          <!-- 二级菜单 -->
+          <el-submenu index='2'>
+            <template slot="title">粉丝管理</template>
+            <el-menu-item>图文数据</el-menu-item>
+            <el-menu-item>粉丝概况</el-menu-item>
+            <el-menu-item>粉丝画像</el-menu-item>
+            <el-menu-item>粉丝列表</el-menu-item>
+          </el-submenu>
+          <el-menu-item>账户信息</el-menu-item>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧 -->
+      <el-container>
+        <el-header>
+          <header-a></header-a>
+        </el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import headerA from '../../components/header.vue'
+export default {
+  name: 'home',
+  components: {
+    'header-a': headerA
+  }
+}
+</script>
+<style lang="less" scoped>
+  .el-aside {
+    background: #323745;
+    height: 100vh;
+    overflow: hidden;
+    .aside {
+      height: 60px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      img {
+        height: 30px;
+      }
+    }
+    // .menu {
+    //   background-color: #353b4e;
+    //   text-color: #adafb5;
+    //   // :active:#aaa
+    // }
+  }
+</style>
