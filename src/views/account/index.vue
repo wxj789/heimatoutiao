5acf1abc8db6203b747863f3237b396adff8e65e
wxj789 <1105747245@qq.com>
2019-11-22 21:24:50 +0800

    账户信息-修改用户资料和用户头像

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -4,22 +4,29 @@
       <div slot="header" class="clearfix">
         <span>账户信息</span>
       </div>
-      <div style="width:400px">
-          <el-form ref="form" :model="accountInfo" label-width="100px">
-            <el-form-item label="昵称：">
-                <el-input v-model="accountInfo.name" ></el-input>
-            </el-form-item>
-            <el-form-item label="个人简介：">
-                <el-input v-model="accountInfo.intro" ></el-input>
-            </el-form-item>
-            <el-form-item label="手机号：">
-                <el-input v-model="accountInfo.mobile" ></el-input>
-            </el-form-item>
-            <el-form-item label="邮箱：">
-                <el-input disabled v-model="accountInfo.email" ></el-input>
-            </el-form-item>
-            <el-button type="primary" style="margin-left:100px">提交修改</el-button>
-          </el-form>
+      <div class="list">
+        <div style="width:400px">
+            <el-form ref="form" :model="accountInfo" label-width="100px">
+                <el-form-item label="昵称：">
+                    <el-input v-model="accountInfo.name" ></el-input>
+                </el-form-item>
+                <el-form-item label="个人简介：">
+                    <el-input v-model="accountInfo.intro" ></el-input>
+                </el-form-item>
+                <el-form-item label="手机号：">
+                    <el-input v-model="accountInfo.mobile" ></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱：">
+                    <el-input disabled v-model="accountInfo.email" ></el-input>
+                </el-form-item>
+                <el-button type="primary" style="margin-left:100px" @click="onConfirm">提交修改</el-button>
+            </el-form>
+        </div>
+        <div class="right"  :model="accountInfo">
+            <img style="width:200px; margin-bottom:20px" :src="accountInfo.photo" alt="">
+            <el-button type="primary" @click="onLoadImage">修改头像</el-button>
+            <input type="file" hidden ref="image" @change="onSelectImage" >
+        </div>
       </div>
     </el-card>
   </div>
@@ -42,8 +49,33 @@ export default {
         method: 'GET',
         url: '/user/profile'
       }).then(res => {
-        console.log(res.data)
+        // console.log(res.data)
         this.accountInfo = res.data.data
+      })
+    },
+    onConfirm () {
+      this.$axios({
+        method: 'PATCH',
+        url: '/user/profile',
+        data: this.accountInfo
+      }).then(res => {
+        // console.log(res.data)
+      })
+    },
+    onLoadImage () {
+      this.$refs.image.click()
+    },
+    onSelectImage () {
+      const file = this.$refs.image.files[0]
+      const fd = new FormData()
+      fd.append('photo', file)
+      this.$axios({
+        method: 'PATCH',
+        url: '/user/photo',
+        data: fd
+      }).then(res => {
+        // console.log(res.data)
+        this.loadInfo()
       })
     }
   }
@@ -51,6 +83,14 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.list {
+    display: flex;
+    justify-content: space-between;
+    .right {
+        display: flex;
+        flex-direction: column;
+        margin-right: 300px;
+    }
+}
 </style>
